a66b3575c4c0e916a3761fe8ce50c28c6fff8896
dinobergueviana <dinobergue@boxti.com.br>
2021-09-16 13:27:26 -0300

    adds RepositoryList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
-  return <h1>Oo2</h1>
+  return <RepositoryList/>
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,34 @@
+const repositoryName = "site cabuloso";
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de respositórios</h1>
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+      </ul>
+    </section>
+  )
+}
