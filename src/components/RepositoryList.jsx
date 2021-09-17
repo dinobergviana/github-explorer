2087057e368619b7955a312f08f3119c60714097
DinobergueViana <dinobergueviana@gmail.com>
2021-09-16 21:24:51 -0300

    finishes RepositoryList component css styles

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,16 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss';
+
 const repository = {
-  title: 'unform',
+  title: 'unform2',
   description: 'Form em React',
   link: "https://github.com/unform/unform"
 };
+
 export function RepositoryList() {
   return (
-    <section>
+    <section className="repository-list">
       <h1>Lista de respositórios</h1>
       <ul>
         <RepositoryItem 
